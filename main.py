dd55df0d33975a34f6b744bfbfe12fb4250f75e0
Arc <john@johnarchuletta>
2017-12-26 10:06:09 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+''' Server for Udacity Front End project '''
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    ''' Route for index page '''
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.run(host='0.0.0.0', port=8080)
