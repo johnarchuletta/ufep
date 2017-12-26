d5c9134b4a57bc9efbbaef1b4175cb987b35d618
Arc <john@johnarchuletta>
2017-12-26 14:06:48 -0700

    Working on Heroku deploy

--- main.py
+++ main.py
@@ -14,5 +14,6 @@ def index():
 
 
 if __name__ == '__main__':
+    port = os.getenv('PORT', 8080)
     app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.run(host='0.0.0.0', port=33507)
+    app.run(host='0.0.0.0', port=port)
